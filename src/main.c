fda72177b4bfd9addba3e6d4639f4e9d46fabb81
Vinicio <pablovglez@gmail.com>
2023-08-12 22:06:23 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,413 @@
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include "camera_pins.h"
+#include "connect_wifi.h"
+
+#include <stdbool.h>
+
+#include <inttypes.h>
+
+static const char *TAG = "Ixtli esp32-cam Websocket server";
+bool led_bool = 0;
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+#define CONFIG_XCLK_FREQ 20000000 
+
+static esp_err_t init_camera(void)
+{
+    camera_config_t camera_config = {
+        .pin_pwdn  = CAM_PIN_PWDN,
+        .pin_reset = CAM_PIN_RESET,
+        .pin_xclk = CAM_PIN_XCLK,
+        .pin_sscb_sda = CAM_PIN_SIOD,
+        .pin_sscb_scl = CAM_PIN_SIOC,
+
+        .pin_d7 = CAM_PIN_D7,
+        .pin_d6 = CAM_PIN_D6,
+        .pin_d5 = CAM_PIN_D5,
+        .pin_d4 = CAM_PIN_D4,
+        .pin_d3 = CAM_PIN_D3,
+        .pin_d2 = CAM_PIN_D2,
+        .pin_d1 = CAM_PIN_D1,
+        .pin_d0 = CAM_PIN_D0,
+        .pin_vsync = CAM_PIN_VSYNC,
+        .pin_href = CAM_PIN_HREF,
+        .pin_pclk = CAM_PIN_PCLK,
+
+        .xclk_freq_hz = CONFIG_XCLK_FREQ,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+
+        .pixel_format = PIXFORMAT_JPEG,
+        .frame_size = FRAMESIZE_VGA,
+
+        .jpeg_quality = 10,
+        .fb_count = 1,
+        .grab_mode = CAMERA_GRAB_WHEN_EMPTY};//CAMERA_GRAB_LATEST. Sets when buffers should be filled
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        return err;
+    }
+    return ESP_OK;
+}
+
+
+
+esp_err_t send_web_page(httpd_req_t *req)
+{
+    const char resp[] = "<h1>Hello World</h1>";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+esp_err_t get_req_handler(httpd_req_t *req)
+{
+    return send_web_page(req);
+}
+
+void do_retransmit(const int sock){
+    int len;
+    char rx_buffer[128];
+
+    do {
+        len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        if (len < 0) {
+            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        } else if (len == 0) {
+            ESP_LOGW(TAG, "Connection closed");
+        } else {
+            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+            ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
+
+            // send() can return less bytes than supplied length.
+            // Walk-around for robust implementation.
+            int to_write = len;
+            while (to_write > 0) {
+                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
+                if (written < 0) {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                    // Failed to retransmit, giving up
+                    return;
+                }
+                to_write -= written;
+            }
+            
+        }
+    } while (len > 0);
+}
+
+void do_transmit(const int sock){
+
+
+    char tx_buffer[128];
+    int counter = 0;
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    int tx_len = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+
+    while (1) {
+
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+        if(res == ESP_OK){
+            //tx_len = send(sock, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY), 0);
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            tx_len = send(sock, (const char *)part_buf, hlen, 0);
+            tx_len  = send(sock, (const char *)_jpg_buf, _jpg_buf_len, 0);
+            ESP_LOGI(TAG, "SOCKET: %u ", tx_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if(tx_len == -1){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+        (uint32_t)(_jpg_buf_len/1024),
+        (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+
+        
+    }
+    last_frame = 0;
+    return res;
+    // Prepare the data to be sent
+    /*
+    snprintf(tx_buffer, sizeof(tx_buffer), "Hello from ESP32! Counter: %d", counter);
+
+    // Send data through the socket
+    int tx_len = send(sock, tx_buffer, strlen(tx_buffer), 0);
+    counter++;
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    */
+
+}
+
+void socket_server_task(void *pvParameters) {
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8081);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    int err = bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        close(listen_sock);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        close(listen_sock);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    ESP_LOGI(TAG, "Socket server started. Waiting for client connections...");
+    
+    int counter = 0;
+
+    while (1) {
+
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+        
+        setsockopt(client_sock, IPPROTO_TCP, TCP_KEEPIDLE, &err, sizeof(int));
+
+        char addr_str[INET_ADDRSTRLEN];
+        inet_ntoa_r(client_addr.sin_addr, addr_str, sizeof(addr_str));
+        ESP_LOGI(TAG, "Client connected from %s:%d", addr_str, ntohs(client_addr.sin_port));
+        
+        //do_retransmit(client_sock);
+        do_transmit(client_sock);
+
+
+        
+        
+        if (listen_sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket...");
+            shutdown(client_sock, 0);
+            close(client_sock);
+        }
+    }
+
+    vTaskDelete(NULL);
+}
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+            (uint32_t)(_jpg_buf_len/1024),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
+
+static esp_err_t flash_led_brightness_handler(httpd_req_t *req)
+{
+    led_bool = (led_bool + 1 ) % 2 ;
+    gpio_set_level(4, led_bool);
+    ESP_LOGI(TAG, "Led is %d\n", led_bool);
+
+    return send_web_page(req);
+}
+
+ 
+
+httpd_uri_t uri_flash = {
+    .uri = "/flash",
+    .method = HTTP_GET,
+    .handler = flash_led_brightness_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_get = {
+    .uri = "/cam",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_home = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = get_req_handler,
+    .user_ctx = NULL};
+
+httpd_handle_t setup_stream_server(void)
+{
+    httpd_config_t stream_config = HTTPD_DEFAULT_CONFIG();
+    stream_config.server_port = 8080;
+    stream_config.ctrl_port = 32770;
+
+    httpd_handle_t stream_httpd  = NULL;
+
+    if (httpd_start(&stream_httpd , &stream_config) == ESP_OK)
+    {
+        httpd_register_uri_handler(stream_httpd , &uri_get);
+    }
+
+    ESP_LOGI(TAG, "ESP32 CAM Web Server is up and running\n");
+    return stream_httpd;
+}
+
+httpd_handle_t setup_api_server(void)
+{
+    httpd_config_t api_config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t api_httpd  = NULL;
+
+    if (httpd_start(&api_httpd , &api_config) == ESP_OK)
+    {
+        httpd_register_uri_handler(api_httpd, &uri_home);
+        httpd_register_uri_handler(api_httpd, &uri_flash);
+    }
+
+    ESP_LOGI(TAG, "API Server is up and running\n");
+    return api_httpd;
+}
+
+void app_main()
+{
+    esp_err_t err;
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    connect_wifi();
+    gpio_reset_pin(4);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(4, GPIO_MODE_OUTPUT);
+
+    if (wifi_connect_status)
+    {
+        err = init_camera();
+        if (err != ESP_OK)
+        {
+            printf("err: %s\n", esp_err_to_name(err));
+            return;
+        }
+        setup_stream_server();
+        setup_api_server();
+        xTaskCreate(socket_server_task, "socket_server_task", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+        
+    }
+    else
+        ESP_LOGI(TAG, "Failed to connected with Wi-Fi, check your network Credentials\n");
+    
+}
